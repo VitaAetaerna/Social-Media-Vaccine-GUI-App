1267326b13b4d26351629e1af07db777383bbc81
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2023-09-28 14:35:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ from kivy.lang import Builder
 from plyer import notification
 from kivymd.uix.button import MDRectangleFlatButton, MDFloatingActionButton
 from kivymd.uix.button import MDIconButton
+import random
 Window.size = (300, 500)
-
-#ICONS https://gitlab.com/kivymd/KivyMD/blob/master/kivymd/icon_definitions.py#L4
-
-#KV FILE
-
-
 
 Personal = '''
 MDFillRoundFlatIconButton:
@@ -43,21 +38,92 @@ MDFillRoundFlatIconButton:
 '''
 
 
+tbar = ''' 
+MDScreen:
 
-#END
+    MDBottomNavigation:
+        #panel_color: "#eeeaea"
+        selected_color_background: "green"
+        text_color_active: "white"
 
-#DESIGN  (KV)
-tbar = """
-Screen:
-    BoxLayout:
-        MDBottomAppBar:
-            MDTopAppBar:
-                icon: "home"
-                type: "bottom"
-                left_action_items: [["human-male-female", lambda y: y]]
-                right_action_items: [["account-settings", lambda y:y]]
+        MDBottomNavigationItem:
+            name: 'screen 1'
+            text: 'Groups'
+            icon: 'group'
+            badge_icon: "numeric-1"
 
-"""
+            MDFillRoundFlatIconButton:
+                text: "                           Private | Personal                    "
+                icon: "information"
+                pos: 0, 260
+                size_hint_y: 0.15
+                width:300
+                height: 250
+
+            MDFillRoundFlatIconButton:
+                text: "                                Public | School                     "
+                icon: "school"
+                pos: 0, 180
+                size_hint_y: 0.15
+                width:300
+                height: 250
+
+            MDFillRoundFlatIconButton:
+                text: "                           Public | Festival                       "
+                icon: "microphone"
+                pos: 0, 100
+                size_hint_y: 0.15
+                width:300
+                height: 250
+
+        MDBottomNavigationItem:
+            name: 'screen 2'
+            text: 'Join Group'
+            icon: 'reply'
+
+
+            MDFillRoundFlatIconButton:
+                text: "          Create New Group                          "
+                icon: "school"
+                pos: 10, 300
+                size_hint_y: 0.1
+
+            MDFillRoundFlatIconButton:
+                text: "          Delete Group                                     "
+                icon: "school"
+                pos: 10, 235
+                size_hint_y: 0.1
+            
+            MDTextField:
+                mode: "round"
+                max_text_length: 4
+                line_color_focus: "green"
+                helper_text: "Enter a Code to join a Group"
+                helper_text_mode: "persistent"
+                hint_text: "Enter Group Code "
+                icon_right: "human-male-female"
+                icon_right_color: app.theme_cls.primary_color
+                pos: 0, 175
+                size_hint_x:None
+                width:300
+
+            MDFillRoundFlatIconButton:
+                text: "          join          "
+                icon: "school"
+                pos: 85, 110
+                size_hint_y: 0.1
+
+
+
+        MDBottomNavigationItem:
+            name: 'screen 3'
+            text: 'Settings'
+            icon: 'account-settings'
+
+            MDLabel:
+                text: 'Settings'
+                halign: 'center'
+'''
 
 
 #END
@@ -70,6 +136,7 @@ class SaveItForLaterApp(MDApp):
         self.theme_cls.primary_palette = "Green"
         self.theme_cls.primary_hue = '300'
         screen = Screen()
+
 
 
 #TXT FIELDS
