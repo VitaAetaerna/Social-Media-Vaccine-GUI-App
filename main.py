7372f3ccb0c3cbd6000d3a3a40d9466f95924430
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2023-10-01 20:02:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivy.core.window import Window
 from kivy.lang import Builder
-from plyer import notification
 from kivymd.uix.button import MDRectangleFlatButton, MDFloatingActionButton
 from kivymd.uix.button import MDIconButton
 import random
@@ -23,10 +22,17 @@ MDScreen:
             icon: 'group'
             badge_icon: "numeric-1"
 
+            
+            MDIconButton:
+                icon: "account-switch"
+                pos: 250, 380
+                md_bg_color: app.theme_cls.primary_color
+                
+                
             MDFillRoundFlatIconButton:
                 text: "                           Your Groups                             "
                 icon: "human-greeting"
-                pos: 0, 340
+                pos: 0, 285
                 size_hint_y: 0.15
                 width:300
                 height: 250
@@ -34,7 +40,7 @@ MDScreen:
             MDFillRoundFlatIconButton:
                 text: "                           Private | Personal                    "
                 icon: "information"
-                pos: 0, 230
+                pos: 0, 205
                 size_hint_y: 0.15
                 width:300
                 height: 250
@@ -42,7 +48,7 @@ MDScreen:
             MDFillRoundFlatIconButton:
                 text: "                                Public | School                     "
                 icon: "school"
-                pos: 0, 150
+                pos: 0, 125
                 size_hint_y: 0.15
                 width:300
                 height: 250
@@ -50,7 +56,7 @@ MDScreen:
             MDFillRoundFlatIconButton:
                 text: "                           Public | Festival                       "
                 icon: "microphone"
-                pos: 0, 60
+                pos: 0, 35
                 size_hint_y: 0.15
                 width:300
                 height: 250
@@ -63,13 +69,13 @@ MDScreen:
 
             MDFillRoundFlatIconButton:
                 text: "          Create New Group                          "
-                icon: "school"
+                icon: "creation"
                 pos: 10, 300
                 size_hint_y: 0.1
 
             MDFillRoundFlatIconButton:
                 text: "          Delete Group                                     "
-                icon: "school"
+                icon: "delete-sweep"
                 pos: 10, 235
                 size_hint_y: 0.1
             
@@ -80,7 +86,7 @@ MDScreen:
                 helper_text: "Enter a Code to join a Group"
                 helper_text_mode: "persistent"
                 hint_text: "Enter Group Code "
-                icon_right: "human-male-female"
+                icon_right: "key"
                 icon_right_color: app.theme_cls.primary_color
                 pos: 0, 175
                 size_hint_x:None
@@ -88,7 +94,7 @@ MDScreen:
 
             MDFillRoundFlatIconButton:
                 text: "          join          "
-                icon: "school"
+                icon: "inbox"
                 pos: 85, 110
                 size_hint_y: 0.1
 
@@ -129,20 +135,36 @@ MDScreen:
                 text: "        Delete Account                                    "
                 md_bg_color: "red"
                 icon: "delete-forever"
-                pos: 10, 25
+                pos: 10, 15
                 size_hint_y: 0.1
+                on_press: app.stop()
 
             MDFillRoundFlatIconButton:
                 text: "        Dark/Light Mode                                 "
                 icon: "theme-light-dark"
+                icon_color: "white"
                 pos: 10, 265
+                size_hint_y: 0.1
+                md_bg_color: "black"
+                theme_text_color: "Custom"
+                text_color: "white"
+                on_release: self.theme_cls.theme_style = "Dark" if self.theme_cls.theme_style == "Light" else "Light"
+            
+
+                
+            MDFillRoundFlatIconButton:
+                text: "        Read AGB                                                "
+                icon: "book-open"
+                pos: 10, 345 
                 size_hint_y: 0.1
 
             MDFillRoundFlatIconButton:
-                text: "        Read AGB                                      "
-                icon: "book-open"
-                pos: 10, 265
+                text: "        Clear My Data                                       "
+                md_bg_color: "red"
+                icon: "database"
+                pos: 10, 70
                 size_hint_y: 0.1
+        
 '''
 
 
@@ -162,8 +184,9 @@ class VaccineApp(MDApp):
 
         self.Toolb = Builder.load_string(tbar)
         screen.add_widget(self.Toolb)
-
+        
 
         return screen
+
 
 VaccineApp().run()
